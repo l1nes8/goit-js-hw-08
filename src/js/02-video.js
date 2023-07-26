be58c7d8784d025d01d20bdae9af1d8c23ad2ba8
l1nes8 <egor1glavatyje@gmail.com>
2023-07-26 10:14:25 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+
+const saveCurrentTimeToLocalStorage = time => {
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    saveCurrentTimeToLocalStorage(data.seconds);
+  }, 1000)
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+
+  player.setCurrentTime(savedTime);
+});
